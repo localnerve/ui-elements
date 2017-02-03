de277350f83c1f1b96448271f7370869b2923e2d
Alex Grant <alex@localnerve.com>
2017-02-03 12:52:56 -0500

    add proper task return, ignore dirs, comments

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,36 @@ const gulp = require('gulp');
 const webpack = require('webpack');
 
 const srcRoot = 'src';
+const ignoreDirs = [
+  'utils'
+];
 const jsBundle = 'bundle.js';
 
+/**
+ * Searches `srcRoot` for top level directories, ignore `utils` directories.
+ *
+ * @returns {Promise} Resolves to an array of toplevel directories under srcRoot.
+ */
 function getSourceDirs () {
   return q.nfcall(fs.readdir, srcRoot)
     .then(files =>
-      files.filter(file => fs.statSync(path.join(srcRoot, file)).isDirectory())
+      files.filter((file) => {
+        const isDir = fs.statSync(path.join(srcRoot, file)).isDirectory();
+        return isDir ? ignoreDirs.indexOf(file) === -1 : false;
+      })
         .map(file => path.join(srcRoot, file))
       );
 }
 
+/**
+ * Returns an array of webpack config objects that assume these constraints:
+ * 1. Produces one .js only bundle named `jsBundle`.
+ * 2. Entry point is index.js
+ *
+ * @param {String} env - The target env, 'production' for production.
+ * @param {Array} dirs - toplevel directories under srcRoot.
+ * @returns {Array} Webpack config objects.
+ */
 function getWebpackConfig (env, dirs) {
   const prod = env === 'production';
   const definitions = {
@@ -57,32 +77,31 @@ function getWebpackConfig (env, dirs) {
   }));
 }
 
+/**
+ * Creates js bundles for all top level directories under `srcRoot`.
+ *
+ * @param {String} env - "production" for production.
+ * @returns {Promise} Resolves to undefined on completion.
+ */
 function createBundle (env) {
-  getSourceDirs()
+  return getSourceDirs()
     .then(getWebpackConfig.bind(null, env))
     .then(webpackConfigs => q.nfcall(webpack, webpackConfigs))
     .then((stats) => {
       if (stats.hasErrors()) {
         throw stats.toJson().errors;
       }
-    })
-    .catch((e) => {
-      console.error(e);
     });
 }
 
-gulp.task('copy', () => {
+// Define the build tasks:
+
+gulp.task('copy', () =>
   gulp.src([
     `${srcRoot}/**/index.html`,
-    `${srcRoot}/**/${jsBundle}`,
+    `${srcRoot}/**/${jsBundle}`
   ])
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('webpack', () => {
-  createBundle('production');
-});
-
-gulp.task('webpack-dev', () => {
-  createBundle('development');
-});
+    .pipe(gulp.dest('dist'))
+);
+gulp.task('webpack', () => createBundle('production'));
+gulp.task('webpack-dev', () => createBundle('development'));
