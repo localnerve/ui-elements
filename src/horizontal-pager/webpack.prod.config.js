d94e9b48544aad4cf737b28d83c4105638f92faf
Alex Grant <alex@localnerve.com>
2017-04-22 01:08:11 -0400

    npm package build requirements

---
+++ src/horizontal-pager/webpack.prod.config.js
@@ -0,0 +1,40 @@
+/**
+ * webpack config to build the public module.
+ * Devdeps in outer repo.
+ */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: path.join(__dirname, 'horizontal-pager.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.BannerPlugin({
+      banner: 'horizontal-pager, Copyright (c) 2017 Alex Grant (@localnerve), LocalNerve LLC, BSD-3-Clause',
+      entryOnly: true
+    })
+  ]
+};
