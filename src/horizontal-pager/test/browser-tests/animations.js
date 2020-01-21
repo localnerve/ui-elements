28dca991f6498330d11b096cf6fafd14a2477fae
Alex Grant <alex@localnerve.com>
2020-01-21 16:55:16 -0500

    ui-elements@3.0.0, node >= 10, copyright 2020

--- src/horizontal-pager/test/browser-tests/animations.js
+++ src/horizontal-pager/test/browser-tests/animations.js
@@ -1,7 +1,7 @@
 /**
  * Horizontal-pager in-browser unit tests.
  *
- * Copyright (c) 2017-2019 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyright (c) 2017-2020 Alex Grant (@localnerve), LocalNerve LLC
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
  */
 /* global window, document, before, describe, it */
