e58260bbddef81790516a11ae301015cd81bdaca
Alex Grant <alex@localnerve.com>
2021-02-18 14:45:38 -0500

    ui-elements@3.2.2, copyright and deps update

--- src/horizontal-pager/horizontal-pager.js
+++ src/horizontal-pager/horizontal-pager.js
@@ -2,7 +2,7 @@
  * horizontal-pager
  * A small, fast, no-dep, horizontal pager.
  *
- * Copyright (c) 2017-2020 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyright (c) 2017-2021 Alex Grant (@localnerve), LocalNerve LLC
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
  */
 /* global document, requestAnimationFrame, cancelAnimationFrame */
--- src/simple-scroll-intersection/simple-scroll-intersection.js
+++ src/simple-scroll-intersection/simple-scroll-intersection.js
@@ -3,7 +3,7 @@
  * Use when you have a simple situation/animation where IntersectionObserver
  * doesn't make sense.
  *
- * Copyright (c) 2017-2020 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyright (c) 2017-2021 Alex Grant (@localnerve), LocalNerve LLC
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
  */
 /* global window, document */
